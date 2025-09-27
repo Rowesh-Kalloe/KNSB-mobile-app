5e4e38aa8c7f1f770113ad6a52b361940799f2c9
Rowesh <162052911+Rowesh-Kalloe@users.noreply.github.com>
2025-09-27 15:40:19 +0200

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,8 +2,7 @@ import { Tabs } from 'expo-router';
 import { useRouter, useSegments, usePathname } from 'expo-router';
 import { useState, useEffect } from 'react';
 import { GestureDetector, Gesture } from 'react-native-gesture-handler';
-import { View, Dimensions } from 'react-native';
-import { TouchableOpacity, Text } from 'react-native';
+import { View, Dimensions, TouchableOpacity, Text } from 'react-native';
 import Animated, { 
   useSharedValue, 
   useAnimatedStyle, 
@@ -211,7 +210,7 @@ export default function TabLayout() {
             Punten
           </Text>
         </TouchableOpacity>
-      }
+      </View>
     </View>
   );
 }
